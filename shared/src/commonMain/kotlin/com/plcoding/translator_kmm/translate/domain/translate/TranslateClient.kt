54c97fd2f739d5323bd13785870f3a8754d19f91
Aysun Emir <aysun.emir@gmail.com>
2025-01-13 01:07:30 +0300

    Implement translate client and data layer
    - Introduce `TranslateClient` interface and `KtorTranslateClient` implementation for handling translation requests.
    - Add `TranslateDto`, `TranslatedDto` for handling request and response respectively.
    - Define `Language` enum to represent supported languages.
    - Introduce `TranslateError` enum to handle translation error.
    - Add `HttpClientFactory` to provide cross platform HTTP Client.
    - Implement `TranslateException` for translation request exception handling.
    - Add `NetworkConstants` for base url.

---
+++ shared/src/commonMain/kotlin/com/plcoding/translator_kmm/translate/domain/translate/TranslateClient.kt
@@ -0,0 +1,15 @@
+package com.plcoding.translator_kmm.translate.domain.translate
+
+import com.plcoding.translator_kmm.core.domain.language.Language
+
+interface TranslateClient {
+    suspend fun translate(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): String
+}
+
+class TranslateException(error: TranslateError) : Exception(
+    message = "An error occurred when translating: $error"
+)
---
+++ shared/src/commonMain/kotlin/com/plcoding/translator_kmm/translate/domain/translate/TranslateError.kt
@@ -0,0 +1,8 @@
+package com.plcoding.translator_kmm.translate.domain.translate
+
+enum class TranslateError {
+    SERVICE_UNAVAILABLE,
+    CLIENT_ERROR,
+    SERVER_ERROR,
+    UNKNOWN_ERROR
+}
