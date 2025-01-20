beadbbdaa055d90c805b8fd1eee9e7a743bacef3
Aysun Emir <aysun.emir@gmail.com>
2025-01-20 23:30:26 +0300

    ``` Implement custom app theming for Android
    - Define a custom color palette in `shared/src/commonMain/kotlin/com/plcoding/translator_kmm/core/presentation/Colors.kt` with colors like `LightBlue`, `LightBlueGrey`, `AccentViolet`, `TextBlack`, and `DarkGrey` for use in light and dark themes.
    - Create light and dark color schemes in `androidApp/src/main/java/com/plcoding/translator_kmm/android/core/theme/Color.kt`.
    - Create `TranslatorTheme` in `androidApp/src/main/java/com/plcoding/translator_kmm/android/MainActivity.kt` using `MaterialTheme.colors.background`.
    - Add font family to `TranslatorTheme` with support for `sf_pro_text_regular`, `sf_pro_text_medium` and `sf_pro_text_bold`.
    - Update `TranslatorTheme` `Typography` with font family and font size to set up different text styles.
    - Update MainActivity to set up `TranslatorTheme` with composable `Surface` and `MaterialTheme.colors.background` as background.
    - Update `Greeting` composable to receive text parameter from `Greeting().greeting()`.
    ```

--- androidApp/src/main/java/com/plcoding/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/plcoding/translator_kmm/android/MainActivity.kt
@@ -9,38 +9,59 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.plcoding.translator_kmm.Greeting
+import com.plcoding.translator_kmm.android.core.theme.darkColors
+import com.plcoding.translator_kmm.android.core.theme.lightColors
 
 @Composable
-fun MyApplicationTheme(
+fun TranslatorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        darkColors(
-            primary = Color(0xFFBB86FC),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
+        darkColors
     } else {
-        lightColors(
-            primary = Color(0xFF6200EE),
-            primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
-        )
+        lightColors
     }
+
+    val sfProText = FontFamily(
+        Font(R.font.sf_pro_text_regular, FontWeight.Normal),
+        Font(R.font.sf_pro_text_medium, FontWeight.Medium),
+        Font(R.font.sf_pro_text_bold, FontWeight.Bold),
+    )
+
     val typography = Typography(
+        h1 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        ),
+        h3 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp
+        ),
         body1 = TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = sfProText,
             fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
+            fontSize = 14.sp
+        ),
+        body2 = TextStyle(
+            fontFamily = sfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
         )
     )
     val shapes = Shapes(
@@ -61,27 +82,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            TranslatorTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting(Greeting().greeting())
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Hello, Android!")
     }
 }
