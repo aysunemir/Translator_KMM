453489ec82eb031b2e43e30612c980c3de9dd49d
Aysun Emir <aysun.emir@gmail.com>
2025-01-23 00:16:13 +0300

    Implement Translate Screen UI
    This commit implements the UI for the translate screen, including:
    
    - Displaying text fields for source and target languages.
    - Adding buttons for translation, copying text, and closing the translation.
    - Showing a progress indicator while translating.
    - Implementing a gradient background for the text fields.
    - Adding language icons to the language dropdowns.

--- androidApp/src/main/java/com/plcoding/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/plcoding/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,5 +1,6 @@
 package com.plcoding.translator_kmm.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -7,14 +8,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import com.plcoding.translator_kmm.android.R
 import com.plcoding.translator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.plcoding.translator_kmm.android.translate.presentation.components.SwapLanguagesButton
+import com.plcoding.translator_kmm.android.translate.presentation.components.TranslateTextField
 import com.plcoding.translator_kmm.translate.presentation.TranslateEvent
 import com.plcoding.translator_kmm.translate.presentation.TranslateState
 
@@ -24,6 +30,7 @@ fun TranslateScreen(
     onEvent: (TranslateEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
         }
@@ -62,7 +69,46 @@ fun TranslateScreen(
                     )
                 }
             }
+            item {
+                val clipBoardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipBoardManager.setText(
+                            buildAnnotatedString {
+                                append(text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.copied_to_clipboard),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         }
     }
-
 }
